48d10a776c33e16c5078648ba54744fbe3c8d879
Pablo Gutierrez <pablo@vivalacloud.com>
2014-08-14 15:41:02 +0200

    issue in Forms dont validate

--- src/data/assets/js/login/login.js
+++ src/data/assets/js/login/login.js
@@ -26,64 +26,63 @@ $(document).ready(function(){
 				jQuery.each(response.data,function(key,value){
 					$(".ajax-loader-"+key).html(value);
 				});
+				$("#form-login").submit(function(e){
+							e.preventDefault();
+				});
+
+				$("#form-login").validate({
+					messages:{
+						email:{
+							required: $s["login_email_this_field_is_compulsory"],
+							email: $s["login_email_format_is_not_correct"]
+						},
+						password:{
+							required: $s["login_password_this_field_is_compulsory"]
+						}
+					},
+					rules:{
+						email:{
+							required:true,
+							email: true
+						},
+						password:{
+							required:true
+						}
+					},
+					submitHandler:function(form){
+						$('#form-login .form-error').remove();
+
+						$.ajax({
+							type: 'GET',
+							jsonp: 'callback',
+					    async: false,
+							dataType: 'jsonp',
+					    jsonpCallback: 'jsonCallback',
+							contentType: 'application/json',
+							url: $SERVER_PATH+"server/ajax/login/check_login.php",
+							data: {
+								"email":$('#form-login #email').val(),
+								"password":$('#form-login #password').val()
+							},
+							error: function(data, textStatus, jqXHR) {
+								form_error_handler("form-login",$s_form_error["ajax_error"]);
+							},
+							success: function(response) {
+								if(response.result){
+									localStorage.setItem('id_user',response.data.id_user);
+									localStorage.setItem('sessionkey',response.data.sessionkey);
+									window.location.href = "../welcome/index.html";
+								}else{
+									form_error_handler("form-login",response.error_code_str);
+								}
+							}
+
+						});
+					}
+				});
 			}else{
 				error_handler(response.error_code);
 			}
-		}
-	});
-
-	$("#form-login").submit(function(e){
-				e.preventDefault();
-	});
-
-	$("#form-login").validate({
-		messages:{
-			email:{
-				required: $s["login_email_this_field_is_compulsory"],
-				email: $s["login_email_format_is_not_correct"]
-			},
-			password:{
-				required: $s["login_password_this_field_is_compulsory"]
-			}
-		},
-		rules:{
-			email:{
-				required:true,
-				email: true
-			},
-			password:{
-				required:true
-			}
-		},
-		submitHandler:function(form){
-			$('#form-login .form-error').remove();
-
-			$.ajax({
-				type: 'GET',
-				jsonp: 'callback',
-		    async: false,
-				dataType: 'jsonp',
-		    jsonpCallback: 'jsonCallback',
-				contentType: 'application/json',
-				url: $SERVER_PATH+"server/ajax/login/check_login.php",
-				data: {
-					"email":$('#form-login #email').val(),
-					"password":$('#form-login #password').val()
-				},
-				error: function(data, textStatus, jqXHR) {
-					form_error_handler("form-login",$s_form_error["ajax_error"]);
-				},
-				success: function(response) {
-					if(response.result){
-						localStorage.setItem('id_user',response.data.id_user);
-						localStorage.setItem('sessionkey',response.data.sessionkey);
-						window.location.href = "../welcome/index.html";
-					}else{
-						form_error_handler("form-login",response.error_code_str);
-					}
-				}
-
-			});
 		}
 	});
 });
--- src/data/assets/js/signup/signup.js
+++ src/data/assets/js/signup/signup.js
@@ -5,119 +5,113 @@
   * Version: 0.03
   *
   *********************************************************/
-
-  $(document).ready(function(){
-    $.ajax({
-      async: false,
-      type: 'GET',
-      dataType: 'jsonp',
-      jsonp: 'callback',
-      jsonpCallback: 'jsonCallback',
-      contentType: 'application/json',
-      url: $SERVER_PATH+"server/ajax/signup/get_signup.php",
-      data: {
-        lang: localStorage.getItem("lang")
-      },
-      error: function(data, textStatus, jqXHR) {
-        error_handler("ajax_error");
-      },
-      success: function(response) {
-        if(response.result){
-          jQuery.each(response.data,function(key,value){
-             $(".ajax-loader-"+key).html(value);
-          });
-        }else{
-          error_handler(response.error_code);
-        }
-      }
-    });
-
-    $("#form-signup").submit(function(e){
-          e.preventDefault();
-    });
-
-    $("#form-signup").validate({
-      messages:{
-        name:{
-					required: $s["signup_name_this_field_is_compulsory"],
-					maxlength: $s["signup_name_it_canot_be_longer_than_100_characters"],
-					minlength: $s["signup_name_this_field_needs_4_character_minimum"]
-  			},
-  			email:{
-  				remote: $s["signup_email_this_email_is_already_registered"],
-  				required: $s["signup_email_this_field_is_compulsory"],
-  				email: $s["signup_email_format_is_not_correct"]
-  			},
-        password:{
-  				required: $s["signup_password_this_field_is_compulsory"],
-  				maxlength: $s["signup_password_it_canot_be_longer_than_25_characters"],
-  				minlength: $s["signup_password_this_field_needs_8_character_minimum"]
-  			},
-  			repeat_password:{
-  				required:$s["signup_repeat_password_this_field_is_compulsory"],
-  				equalTo: $s["signup_repeat_password_both_passwords_do_not_coincide"]
-  			},
-  			accept_policy:{
-  				required: $s["signup_it_is_necessary_to accept_privacy_policy"],
-  			}
-      },
-      rules:{
-        name:{
-  				required:true,
-  				maxlength: 100,
-  				minlength: 4
-  			},
-  			email:{
-  				remote:$SERVER_PATH+"server/ajax/signup/check_email_not_used.php",
-  				required:true,
-  				email: true
-  			},
-  			password:{
-  				required:true,
-  				maxlength: 25,
-  				minlength: 8
-  			},
-  			repeat_password:{
-  				required:true,
-  				equalTo:"#password"
-  			},
-  			accept_policy:{
-  				required: true
-  			}
-      },
-      submitHandler:function(form){
-        $('#form-signup .form-error').remove();
-
-        $.ajax({
-          type: 'GET',
-          jsonp: 'callback',
-          async: false,
-          dataType: 'jsonp',
-          jsonpCallback: 'jsonCallback',
-          contentType: 'application/json',
-          url: $SERVER_PATH+"server/ajax/signup/add_signup.php",
-          data: {
-            "name":$('#form-signup #name').val(),
-            "email":$('#form-signup #email').val(),
-            "phone":$('#form-signup #phone').val(),
-            "password":$('#form-signup #password').val()
-          },
-          error: function(data, textStatus, jqXHR) {
-            form_error_handler("form-signup",$s_form_error["ajax_error"]);
-          },
-          success: function(response) {
-            alert("ok");
-
-            if(response.result){
-            }else{
-              alert("error");
-              alert(response.error_code_str);
-              form_error_handler("form-signup",response.error_code_str);
-            }
-          }
-
+  $.ajax({
+    type: 'GET',
+    dataType: 'jsonp',
+    jsonp: 'callback',
+    jsonpCallback: 'jsonCallback',
+    contentType: 'application/json',
+    url: $SERVER_PATH+"server/ajax/signup/get_signup.php",
+    data: {
+      lang: localStorage.getItem("lang")
+    },
+    error: function(data, textStatus, jqXHR) {
+      error_handler("ajax_error");
+    },
+    success: function(response) {
+      if(response.result){
+        jQuery.each(response.data,function(key,value){
+           $(".ajax-loader-"+key).html(value);
         });
-      }
-    });
+        $("#form-signup").submit(function(e){
+              e.preventDefault();
+        });
 
+        $("#form-signup").validate({
+          messages:{
+            name:{
+              required: $s["signup_name_this_field_is_compulsory"],
+              maxlength: $s["signup_name_it_canot_be_longer_than_100_characters"],
+              minlength: $s["signup_name_this_field_needs_4_character_minimum"]
+            },
+            email:{
+              remote: $s["signup_email_this_email_is_already_registered"],
+              required: $s["signup_email_this_field_is_compulsory"],
+              email: $s["signup_email_format_is_not_correct"]
+            },
+            password:{
+              required: $s["signup_password_this_field_is_compulsory"],
+              maxlength: $s["signup_password_it_canot_be_longer_than_25_characters"],
+              minlength: $s["signup_password_this_field_needs_8_character_minimum"]
+            },
+            repeat_password:{
+              required:$s["signup_repeat_password_this_field_is_compulsory"],
+              equalTo: $s["signup_repeat_password_both_passwords_do_not_coincide"]
+            },
+            accept_policy:{
+              required: $s["signup_it_is_necessary_to accept_privacy_policy"],
+            }
+          },
+          rules:{
+            name:{
+              required:true,
+              maxlength: 100,
+              minlength: 4
+            },
+            email:{
+              remote:$SERVER_PATH+"server/ajax/signup/check_email_not_used.php",
+              required:true,
+              email: true
+            },
+            password:{
+              required:true,
+              maxlength: 25,
+              minlength: 8
+            },
+            repeat_password:{
+              required:true,
+              equalTo:"#password"
+            },
+            accept_policy:{
+              required: true
+            }
+          },
+          submitHandler:function(form){
+            $('#form-signup .form-error').remove();
+
+            $.ajax({
+              type: 'GET',
+              jsonp: 'callback',
+              async: false,
+              dataType: 'jsonp',
+              jsonpCallback: 'jsonCallback',
+              contentType: 'application/json',
+              url: $SERVER_PATH+"server/ajax/signup/add_signup.php",
+              data: {
+                "name":$('#form-signup #name').val(),
+                "email":$('#form-signup #email').val(),
+                "phone":$('#form-signup #phone').val(),
+                "password":$('#form-signup #password').val()
+              },
+              error: function(data, textStatus, jqXHR) {
+                form_error_handler("form-signup",$s_form_error["ajax_error"]);
+              },
+              success: function(response) {
+                alert("ok");
+
+                if(response.result){
+                }else{
+                  alert("error");
+                  alert(response.error_code_str);
+                  form_error_handler("form-signup",response.error_code_str);
+                }
+              }
+
+            });
+          }
+        });
+      }else{
+        error_handler(response.error_code);
+      }
+    }
   });
